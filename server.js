57460543ffdbd037ebf968d9a322207d999aea45
Martin Aguero <martin.aguero84@gmail.com>
2024-02-20 15:26:26 -0600

    added to the server.js file, add the promptUser() function with the possible action questions and cases to execute a particular function based on the users selection

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const cTable = require('console.table');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+//Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'ninja1234',
+        database: 'mycompany_db'
+    },
+    console.log('Connected to the mycompany_db database')
+);
+
+//Function to Prompt the user on what action they want to take
+function promptUser() {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+        ]
+    }).then(answer => {
+        switch (answer.action) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                db.end();
+                break;
+            default:
+                console.log(`Invalid action: ${answer.action}`);
+                promptUser();
+        }
+    });
+}
+
+//Start the Prompt
+promptUser();
+
+
+
+//Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on https://localhost:${PORT}`);
+});
